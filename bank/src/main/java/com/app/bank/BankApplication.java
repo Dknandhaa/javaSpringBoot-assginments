44043983c78fe9f661fa13e51e6cea1695a33dca
NANDHA KUMAR D <157778509+Dknandhaa@users.noreply.github.com>
2024-06-28 12:35:22 +0530

    BankApplication.java

--- bank/src/main/java/com/app/bank/BankApplication.java
+++ bank/src/main/java/com/app/bank/BankApplication.java
@@ -3,7 +3,7 @@ package com.app.bank;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
+@EnableScheduling
 @SpringBootApplication
 public class BankApplication {
 
